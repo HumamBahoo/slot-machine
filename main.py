ac27d914db238017d67b1df973b6b07ce4c3e6a8
Humam Bahoo <humam.bahoo@gmail.com>
2023-09-24 23:26:42 -0400

    update display_reels and rename methods

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def main():
 
     machine = SlotMachine()
 
-    machine.set_lines_count()
+    machine.prompt_for_rows_count()
     machine.add_to_balance()
-    machine.set_bet_amount()
-    machine.set_selected_lines_count()
+    machine.prompt_for_bet_amount()
+    machine.prompt_for_selected_lines_count()
 
     clear_terminal()
 
@@ -25,15 +25,27 @@ def main():
     input("Press [Enter] to spin")
 
     while True:
-        clear_terminal()
-
         machine.spin()
 
-        print(f"Balance: ${machine.balance}, Bet Amount: ${machine.bet_amount}, Selected Lines: {machine.selected_lines_count}\n")
+        winning_lines = machine.get_winning_lines()
+        amount_won = machine.calculate_winning(winning_lines)
 
+        machine.update_balance(amount_won)
+        machine.update_total_winnings(amount_won)
+
+        clear_terminal()
+
+        print(f"Balance: ${machine.balance}, Bet Amount: ${machine.bet_amount}, Selected Lines: {machine.selected_lines_count}\n")
         print(f"Total Bet: ${machine.selected_lines_count * machine.bet_amount}, Total Winning: ${machine.total_winnings}\n")
 
-        machine.display_reels()
+        machine.display_reels(winning_lines)
+        print("")
+
+        if amount_won > 0:
+            print(f"You've won! Total prize: ${amount_won}")
+        else:
+            print(f"You've lost!")
+
         print("")
 
         input("Press [Enter] to spin again.\n")
--- modules/slot_machine.py
+++ modules/slot_machine.py
@@ -4,22 +4,22 @@ import random
 class SlotMachine:
     def __init__(self) -> None:
         self.title: str = "Basic Slot Machine"
-        self.symbols_weight = {"A": 5, "B": 10, "C": 15, "D": 30, "E": 40}
-        self.payout_table = {"A": 50, "B": 25, "C": 20, "D": 15, "E": 10}
+        self.symbols_weight: dict = {"A": 5, "B": 10, "C": 15, "D": 30, "E": 40}
+        self.payout_table: dict = {"A": 50, "B": 25, "C": 20, "D": 15, "E": 10}
         self.rows_count: int = 3
-        self.min_rows_count = 1
-        self.max_rows_count = 5
+        self.min_rows_count: int = 1
+        self.max_rows_count: int = 5
         self.reels_count: int = 3
         self.bet_amount: float = 1
         self.min_bet_amount: float = 1
         self.max_bet_amount: float = 100
         self.selected_lines_count: int = 1
-        self.balance = 0
+        self.balance: float = 0
         self.player_credit: str = 1000.00
         self.reels: list = []
         self.total_winnings: float = 0
 
-    def set_rows_count(self) -> None:
+    def prompt_for_rows_count(self) -> None:
         """
         Prompts user to select number of rows the slot machine has within the allowed range (1 to 5)
         """
@@ -37,7 +37,7 @@ class SlotMachine:
             except ValueError:
                 print("Invalid input. Please enter valid a number.")
 
-    def set_bet_amount(self) -> None:
+    def prompt_for_bet_amount(self) -> None:
         """
         Prompts user to set the bet amount the would like to place per line within the allowed range ($1 to $100).
         It will validate if there is enough balance to successfully set the desired bet amount.
@@ -62,7 +62,7 @@ class SlotMachine:
         else:
             print("Your balance is $0. Add more funds and try again later.")
 
-    def set_selected_lines_count(self) -> None:
+    def prompt_for_selected_lines_count(self) -> None:
         """
         Prompts user to select the number of lines they want to play.
         """
@@ -104,13 +104,6 @@ class SlotMachine:
                 print("Invalid input. Please enter a valid number.")
 
     def spin(self) -> None:
-        self.spin_reels()
-        winning_lines = self.get_winning_lines()
-        amount = self.calculate_winning(winning_lines)
-        self.update_balance(amount)
-        self.update_total_winnings(amount)
-
-    def spin_reels(self) -> None:
         """
         Simulates spinning of slot machine reels.
         """
@@ -179,33 +172,26 @@ class SlotMachine:
         Displays the current state of slot machine reels and the current winning lines if provided
         """
 
-        # transpose and print details
+        # iterate through each line to the number of lines
         for i in range(self.rows_count):
-            for j in range(self.reels_count):
-                # if this is not the last slot
-                if j != self.reels_count - 1:
-                    # if this is the first slot from the line
-                    if j == 0:
-                        if i < self.selected_lines_count:
-                            print(f"Line {i+1} [selected]: {self.reels[j][i]}", end=" | ")
+            # get all line symbols by iterating through each reel and extract the symbol at the current line.
+            line_symbols = []
+            for reel in self.reels:
+                line_symbols.append(reel[i])
 
-                        else:
-                            print(f"Line {i+1}           : {self.reels[j][i]}", end=" | ")
+            # create lines symbols string
+            line_symbols_string = " | ".join(line_symbols)
 
-                    # not the first slot
-                    else:
-                        print(self.reels[j][i], end=" | ")
+            # set line status string based on how many lines has been selected
+            line_status_string = "[selected]" if i < self.selected_lines_count else f"{' ' * len('[selected]')}"
 
-                # last slot in line
-                else:
-                    # check if there is a win on this line
-                    if i in winning_lines:
-                        print(self.reels[j][i], end=" => Win: $")
+            # set win amount string if there is a win.
+            # winning_lines key holds the value of the winning line number
+            # winning_lines value holds the winning line symbol
+            winning_amount_string = ""
 
-                        symbol_payout = self.payout_table[winning_lines[i]] * self.bet_amount
+            if i in winning_lines:
+                winning_amount_string = f" => Win: ${self.payout_table[winning_lines[i]] * self.bet_amount}"
 
-                        print(symbol_payout)
-
-                    # print last slot only
-                    else:
-                        print(self.reels[j][i])
+            # print the result
+            print(f"Line {i+1} {line_status_string}: {line_symbols_string} {winning_amount_string}")
